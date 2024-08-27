5294cf7bf590ac83f5d8916b6c5a22bc23fdc837
damiansweet <damiansweet303@gmail.com>
2024-08-27 10:57:24 -0600

    created read_book func

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def main():
+    book = "./books/frankenstein.txt"
+    book_contents = read_book(book)
+def read_book(book):
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents
 
 main()
