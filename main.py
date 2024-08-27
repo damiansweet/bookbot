e5dd0855c3ec5fd52e1a728e15d4f51565989b88
damiansweet <damiansweet303@gmail.com>
2024-08-27 11:00:37 -0600

    created count_words func

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def main():
     book = "./books/frankenstein.txt"
     book_contents = read_book(book)
+    word_count = count_words(book_contents)
 def read_book(book):
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         return file_contents
+def count_words(book_str):
+    words = book_str.split()
+    return len(words)
 
 main()
